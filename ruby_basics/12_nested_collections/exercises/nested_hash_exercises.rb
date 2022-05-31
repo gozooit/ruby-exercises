22d5b86a9465928bdbb81acf2078f7b3f7d44f4f
gozooit <gozoo.it@protonmail.com>
2022-05-31 14:45:09 +0200

    pending

--- ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
+++ ruby_basics/12_nested_collections/exercises/nested_hash_exercises.rb
@@ -41,7 +41,7 @@ end
 def find_beautiful_languages(languages)
   # Take languages and return a hash containing only languages which have the
   # key/value pair { is_beautiful?: true } listed in their information
-  
+  languages.map { |key,value| value[:is_beautiful?] == true }
 end
 
 def find_language_facts(languages, language_name, fact_index = 0)
